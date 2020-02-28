8c3b9e4242e7660ab16ce5efdfc09f412135fea1
gnanamadhu <madhu@betatestsolutions.com>
2020-02-28 20:49:13 +0530

    Update test cases for `hello command`

--- test/commands/hello.test.ts
+++ test/commands/hello.test.ts
@@ -1,17 +1,35 @@
 import { expect, test } from '@oclif/test';
 
-describe('hello', () => {
-  test
-    .stdout()
+const getStdOut = test.stdout();
+
+describe('hello command', () => {
+  getStdOut
     .command(['hello'])
-    .it('runs hello', context => {
-      expect(context.stdout).to.contain('hello world');
+    .it('should expect hello world', context => {
+      expect(context.stdout).to.equal('hello world from ./src/commands/hello.ts\n');
     });
 
-  test
-    .stdout()
-    .command(['hello', '--name', 'jeff'])
-    .it('runs hello --name jeff', context => {
-      expect(context.stdout).to.contain('hello jeff');
+  getStdOut
+    .command(['hello', '--name', 'madhu'])
+    .it('should expect hello madhu', context => {
+      expect(context.stdout).to.equal('hello madhu from ./src/commands/hello.ts\n');
+    });
+
+  getStdOut
+    .command(['hello', './package.json', '-f'])
+    .it('should expect hello world with force flag and file name', context => {
+      expect(context.stdout).to.equal(
+        'hello world from ./src/commands/hello.ts\n' +
+      'you input --force and --file: ./package.json\n'
+      );
+    });
+
+  getStdOut
+    .command(['hello', './package.json', '-f', '--name', 'madhu'])
+    .it('should expect hello madhu with force flag and file name', context => {
+      expect(context.stdout).to.equal(
+        'hello madhu from ./src/commands/hello.ts\n' +
+      'you input --force and --file: ./package.json\n'
+      );
     });
 });
